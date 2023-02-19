52475d5ff3990dc72d6fd3f457a661cd1b0bb459
Abdul-Rasheed Yussif <syseini3@gmail.com>
2023-02-19 02:38:28 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+int a, b;
+
+for (a = 0; a < 10; a++)
+{
+for (b = a + 1; b < 10; b++)
+{
+putchar('0' + a);
+putchar('0' + b);
+if (a + b != 17)
+{
+putchar(',');
+}
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
