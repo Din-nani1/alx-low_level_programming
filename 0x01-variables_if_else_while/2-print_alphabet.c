f579fd42c2cee1332f60ef3acc4f67b19a805eac
Abdul-Rasheed Yussif <syseini3@gmail.com>
2023-02-18 21:39:02 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - entry of main code
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
